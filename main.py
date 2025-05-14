ecce9a69386726230d1a26a19aa77a6efb36cdc6
Daniel <Danielcoelh77@gmail.com>
2025-05-14 19:17:04 -0300

    Primeiro commit da API

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Olá, mundo! Esta é minha primeira API no Render."}
+
+@app.get("/chat")
+def chat(mensagem: str):
+    resposta = f"Você disse: {mensagem}"
+    return {"resposta": resposta}
